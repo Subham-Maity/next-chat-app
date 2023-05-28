d69fce3abd305de52a5333b175058ad8995c99c6
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-05-28 21:01:31 +0530

    Button and message set

--- client/app/containers/Sender.tsx
+++ client/app/containers/Sender.tsx
@@ -32,7 +32,7 @@ export const Sender = () => {
 
         <div
             className="bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-sky-300/50 to-indigo-900/45 p-4 border-t border-cyan-400 flex items-center pointer-events-none "
-            style={{bottom: 20, borderRadius: "1rem"}}>
+            style={{ marginLeft: "1rem", marginRight: "0.5rem" , bottom: 20, borderRadius: "0.5rem"}}>
 
             <motion.textarea
                 className="w-full p-4 text-base rounded-lg shadow-lg bg-stone-300 pointer-events-auto"
